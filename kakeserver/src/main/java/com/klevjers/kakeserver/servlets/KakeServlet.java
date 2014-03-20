7945163e6ed29411220a5fc372af1fbc53df5aa8
hennikl <hennikl@ifi.uio.no>
2014-03-20 21:32:28 +0100

    Initial blank servlet

---
+++ kakeserver/src/main/java/com/klevjers/kakeserver/servlets/KakeServlet.java
@@ -0,0 +1,23 @@
+package com.klevjers.kakeserver.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class KakeServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		
+		
+		resp.sendError(
+				req.getProtocol().endsWith("1.1") ? HttpServletResponse.SC_METHOD_NOT_ALLOWED
+						: HttpServletResponse.SC_BAD_REQUEST,
+				"Unsupported operation");
+	}
+
+}
---
+++ kakeserver/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<html>
+<head>
+<title>Bajs</title>
+</head>
+<body bgcolor="white">
+	<h1>KakeServer</h1>
+	<% String x = "x";
+		out.println(x);
+	%>
+</body>
+</html>
